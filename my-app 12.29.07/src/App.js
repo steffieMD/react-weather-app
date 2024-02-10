05b679bc4e7d6ddd1fd5585e7fac333005ca1345
Stephanie Metieh <stephaniemetieh@gmail.com>
2024-02-10 08:18:17 +0100

    create a weather app using react

---
+++ my-app 12.29.07/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Footer from "./Footer";
+import Main from "./Main";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container col-sm-8 bg-white rounded-4 mt-5 p-3">
+        <Main />
+        <Footer />
+      </div>
+    </div>
+  );
+}
+
+export default App;
